def8232c8389c11de8349ed10199a72e3592197e
zxp <584770953@qq.com>
2022-03-01 21:49:41 +0800

    留言板

---
+++ src/main/java/com/homework/config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+/**
+ * (C) Copyright Company 2018. All rights reserved.
+ * 文件名：MybatisPlusConfig
+ * 版 本：1.0
+ * 内容简述：Mybatis Plus配置
+ * 创建日期：2018.09.12-13:00:13
+ * 创建人：陈玉麟
+ * 修改记录：
+ * 日期  版本  修改人  修改内容
+ */
+package com.bi.api.common.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@Configuration
+public class MybatisPlusConfig {
+    /**
+     * 设置分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
